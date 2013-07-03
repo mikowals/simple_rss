008abfb05b1e9208bf4a541d805833b98a51d69c
mikowals <mikowals@gmail.com>
2013-07-03 09:57:08 +0000

    made sure to add pubsub.js file

---
+++ packages/pubsubhubbub/pubsub.js
@@ -0,0 +1,181 @@
+var request = Npm.require("request"),
+    http = Npm.require("http"),
+    urllib = Npm.require("url"),
+    Stream = Npm.require("stream").Stream,
+    utillib = Npm.require("util");
+ //   NodePie = require("nodepie");
+
+// Expose to the world
+
+/**
+ * Create a PubSubHubbub client handler object. HTTP server is set up to listen 
+ * the responses from the hubs.
+ *
+ * @constructor
+ * @param {Object} options Options object to set up the handler
+ * @param {String} options.callbackServer Hostname the hubs can send the response, include also the port number if not behind proxy
+ * @param {Number} [options.callbackPath="/hubbub"] Path to listen for (other paths return 404)
+ * @param {String} [options.token] Secret token for the hub to authenticate itself, not used if not set 
+ * @param {Number} [options.maxRSSSize=3MB] Maximum file size in bytes for an incoming RSS file - if longer, it will be trimmed
+ * @param {String} [options.gid] If started as root, downgrade to selected group ID
+ * @param {String} [options.giu] If started as root, downgrade to selected user ID
+ */
+PubSubHubbub = function (options){
+    Stream.call(this);
+
+    options = options || {};
+
+    this.port = options.port || 8921;
+    this.callbackServer = options.callbackServer;
+    this.callbackPath = options.callbackPath || "/hubbub";
+    this.token = options.token || false;
+    this.maxRSSSize = options.maxRSSSize || 3 * 1024 * 1024;
+    this.gid = options.gid;
+    this.uid = options.uid;
+
+    this.server = http.createServer(this.serverHandler.bind(this));
+    this.server.on("error", this.onServerError.bind(this));
+    this.server.on("listening", this.onServerListening.bind(this));
+
+    this.server.listen(this.port);
+}
+utillib.inherits(PubSubHubbub, Stream);
+
+PubSubHubbub.prototype.serverHandler = function(req, res){
+    if(req.url.substr(0, this.callbackPath.length) != this.callbackPath){
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end("<!DOCTYPE html><html><head><meta charset=\"utf-8\"/><title>404 Not Found</title></head><body><h1>404 Not Found</h1></body></html>");
+        return;
+    }
+
+    if(req.method == "GET"){
+        return this.serverGETHandler(req, res);
+    }
+
+    if(req.method == "POST"){
+        return this.serverPOSTHandler(req, res);
+    }
+
+    res.writeHead(500, {"Content-Type": "text/html"});
+    res.end("<!DOCTYPE html><html><head><meta charset=\"utf-8\"/><title>500 Internal Server Error</title></head><body><h1>500 Internal Server Error</h1></body></html>");
+}
+
+PubSubHubbub.prototype.onServerError = function(error){
+    if(error.syscall == "listen"){
+        process.stderr.write("Failed to start listening on port " + this.port + " ("+error.code+")\n");
+        process.exit(1);
+    }
+
+    console.log("HTTP Server error");
+    console.log(error);
+}
+
+PubSubHubbub.prototype.onServerListening = function(){
+    if(typeof this.gid != "undefined"){
+        process.setgid(this.gid);
+    }
+
+    if(typeof this.uid != "undefined"){
+        process.setuid(this.uid);
+    }
+
+    console.log("Server listening on port " + this.port + " as " + process.getuid() + ":" + process.getgid());
+
+    this.emit("listen");
+}
+
+PubSubHubbub.prototype.subscribe = function(topic, hub, callback){
+    this.setSubscription("subscribe", topic, hub, callback);
+}
+
+PubSubHubbub.prototype.unsubscribe = function(topic, hub, callback){
+    this.setSubscription("unsubscribe", topic, hub, callback);
+}
+
+PubSubHubbub.prototype.setSubscription = function(mode, topic, hub, callback){
+    var form = {
+            "hub.mode": mode || "subscribe",
+            "hub.verify": "sync",
+            "hub.callback": this.callbackServer + this.callbackPath,
+            "hub.topic": topic
+        },
+        postParams = {
+            url: hub,
+            form: form,
+            encoding: "utf-8"
+        };
+
+    if(this.token){
+        form["hub.verify_token"] = this.token;
+    }
+
+    request.post(postParams, this.pubsubResponse.bind(this, topic, callback));
+}
+
+PubSubHubbub.prototype.pubsubResponse = function(topic, callback, error, response, body){
+    if(error){
+        return callback(error);
+    }
+
+    if(response.statusCode >= 300){
+        return callback(new Error("Invalid response status "+response.statusCode));
+    }
+
+    return callback(null, topic);
+}
+
+PubSubHubbub.prototype.serverGETHandler = function(req, res){
+    var params = urllib.parse(req.url, true, true);
+
+    if(this.token && (params.query['hub.verify_token'] != this.token || !params.query["hub.topic"])){
+        res.writeHead(500, {"Content-Type": "text/html"});
+        res.end("<!DOCTYPE html><html><head><meta charset=\"utf-8\"/><title>500 Internal Server Error</title></head><body><h1>500 Internal Server Error</h1></body></html>");
+        return;
+    }
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(params.query['hub.challenge']);
+
+    var data = {
+        lease: Number(params.query["hub.lease_seconds"] || 0) + Math.round(Date.now()/1000),
+        topic: params.query["hub.topic"]
+    };
+
+    this.emit(params.query["hub.mode"] || "subscribe", data);
+}
+
+PubSubHubbub.prototype.serverPOSTHandler = function(req, res){
+var self = this;
+var feedResult = {};
+var fp = req.pipe( new feedParser());
+fp.on('error', function(err ){
+        console.log(" got feedparser error: " + err);
+        res.error = err;
+        })
+      .on ( 'meta', function ( meta ){
+        //console.log( "feedparser emmitted meta for url: " + url );
+        if (meta !== null ){
+        feedResult.meta  = meta;
+        }
+        })
+      .on('readable', function(){
+          var stream = this, item, doc;
+          while ( item = stream.read() ) {
+
+          feedResult.article = item ;
+          }
+self.emit( 'feed', feedResult);
+          })
+      .on( 'end', function() {
+          //console.log( rssResult ); 
+          });
+
+    req.on("end", (function(){
+        res.writeHead(204, {'Content-Type': 'text/plain; charset=utf-8'});
+        res.end();
+    }).bind(this));
+
+}
+
+
+
---
+++ server/article.js
@@ -0,0 +1,137 @@
+//var existingLinks = _.pluck( Articles.find({}, {link: 1}).fetch(), 'link' );
+Article = function( doc ){
+  var self = this;
+  self.title = null;
+  self.author = null;
+  self.source = null;
+  self.feed_id = null;
+  self.date = null;
+  self.link = null;
+
+  var link = null,
+    sourceUrl,
+    summary = null;
+
+  self.getSourceUrl = function(){
+    return self.source;
+  }
+
+  self.setSourceUrl = function( sourceUrl ){
+    if (self.sourceUrl && typeof self.sourceUrl === "string" ){
+      self.sourceUrl = sourceUrl.toLowerCase();
+    }
+    else{
+      self.sourceUrl = null;
+    }
+  }
+
+  self.getSummary = function(){
+    return self.summary;
+  }
+  self.setSummary = function( summary ){
+    self.summary =  cleanSummary( summary );
+  }
+  if ( doc ) {
+    self.title = doc.title || null;
+    self.author = doc.author ||  null;
+    self.source = doc.source || null;
+    self.feed_id = doc.feed_id || null;
+    self.date = doc.date || null;
+    self.link = doc.link || null;
+    if ( doc.summary ) self.setSummary( doc.summary );
+    if ( doc.sourceUrl ) self.setSourceUrl ( doc.sourceUrl );
+  }
+};
+
+Article.prototype.toDB = function(){
+  if ( this.sourceUrl ) this.setSourceUrl( this.sourceUrl );
+
+  if ( !keepLimitDate || this.date < keepLimitDate ){
+    //console.error ( ( this.title || this.link) + " too old to insert to Articles");
+    return  null;
+  }
+  else{
+    if ( this._id ) { 
+      //console.error ( ( this.title || this.link) + " already in Articles");
+      return  null;
+    }
+    else{
+      var existing = Articles.findOne( {$or:[{ link: this.link },{ link: this.link.toLowerCase()}]} );
+      if ( existing ){
+	//Articles.update( existing._id, {$set: { summary: this.summary, date: this.date, link: this.link } }, function( error, result){
+	// if( error ) console.log( "update of existing article got error: " + error);
+	//});
+	return  null;
+      }
+      else if (! this.feed_id ){
+	if ( this.sourceUrl && this.source ) {
+	  this.feed_id = Feeds.findOne( {$or:[ {url: this.sourceUrl},{url: this.sourceUrl.toLowerCase()}, {title: this.source}, {title: this.source.toLowerCase()}] })._id;
+	}	
+	else if ( this.sourceUrl ) {
+	  this.feed_id = Feeds.findOne( {$or: [{url: this.sourceUrl }, {url: this.sourceUrl.toLowerCase()}]})._id;
+	}
+	else if ( this.source ) {
+	  this.feed_id = Feeds.findOne( {$or:[ { title: this.source }, {title: this.source.toLowerCase()}]})._id;  
+	}
+	else {
+	  console.log (( this.title || this.link) + " Can't insert article without feed_id, sourceUrl, or source" );
+	  return  null;
+	}
+      }
+      var callback = function (error, result) { 
+	if ( error ) {
+	  console.log ("Attempted insert of " + ( this.title || this.link) + " to Articles and got error: " + error);
+	}
+	else{
+	  //console.log( "inserted to DB: " + result);
+	}
+
+      }
+      Articles.insert ( {
+summary: this.summary, 
+feed_id: this.feed_id, 
+title: this.title, 
+source: this.source, 
+link: this.link, 
+date: this.date, 
+author: this.author 
+	}, 
+	callback
+      );
+    }
+  }
+  return true;
+}
+
+Article.prototype.fromNodePieItem = function ( item ){
+
+  this.title =  item.getTitle();
+  this.date = item.getUpdateDate() || new Date();
+  this.author = item.getAuthor();
+  this.link = item.getPermalink() ;
+  this.setSummary ( item.getContents() );
+  return this;
+};
+
+Article.prototype.fromNodePieFeed = function ( feed, itemNum ){
+
+  this.feed_id = feed.feed_id || null;
+  this.source =  feed.getTitle();
+  this.setSourceUrl( feed.url || feed.getPermalink() );
+  var item = feed.getItem( itemNum || 0 );
+  this.fromNodePieItem( item );
+  return this;
+};
+
+Article.prototype.fromFeedParserItem = function( item ){
+
+  this.title = item.title;
+  this.date = item.date || new Date();
+  this.author =item.author;
+  this.link = item.link;
+  this.setSummary( item.description );
+  this.source = item.meta.url;
+  return this;
+};
+
+
