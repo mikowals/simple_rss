11c37506ea2b78914d498e08791b874f77e5b09f
mikowals <mikowals@gmail.com>
2013-10-31 07:32:13 +0000

    newrelic files added

---
+++ packages/newrelic/package.js
@@ -0,0 +1,12 @@
+
+
+Npm.depends({
+            "newrelic": "1.0.0",
+            "bunyan" : "0.22.0",
+            "continuation-local-storage" : "2.4.3"
+
+            });
+
+Package.on_use(function (api) {
+	       api.add_files(["newrelic.js"], 'server');
+	       });
