a362e35e69ebde06b22a4ed39147342bd0c67b8b
mikowals <mikowals@gmail.com>
2014-01-21 20:58:01 +1000

    pubsubhubbub emits feed event so meteor bindEnvironment is in fiber

--- server/simple_rss.js
+++ server/simple_rss.js
@@ -175,7 +175,7 @@ Meteor.startup( function(){
   }
 
   var feedSubscriber = new FeedSubscriber ( options );
-  
+  feedSubscriber.on( 'liveFeed', Meteor.bindEnvironment( readAndInsertArticles, function( error ) { console.log( error ); } ));
 
   process.on('exit', Meteor.bindEnvironment ( function (){
     feedSubscriber.stopAllSubscriptions();
