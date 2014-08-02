d35ec6628b8f232effa5ef162ff61bbfb00d4306
mikowals <mikowals@gmail.com>
2014-08-02 15:43:41 +1000

    make pubsub path variable

--- packages/pubsubhubbub/pubsubhubbub.next.js
+++ packages/pubsubhubbub/pubsubhubbub.next.js
@@ -3,28 +3,29 @@
 
 var PubSubHubbub = Npm.require("pubsubhubbub");
 var resumer = Npm.require("resumer");
+var urlParse = Npm.require( 'url' ).parse;
+
 FeedSubscriber = (options) => {
   var pubsub = PubSubHubbub.createServer( options );
   console.log( pubsub );
 
   WebApp.connectHandlers.stack.splice(0,0,{
-      route: '/hubbub',
-      handle: function(req, res, next) {
-       if(req.method === 'POST') {
-         return  pubsub._onPostRequest( req, res);
-       }
-       else if(req.method === 'GET') {
-         return pubsub._onGetRequest( req, res);
-     } else {
-         return pubsub._sendError(req, res, 405, "Method Not Allowed");
-     }
+    route: urlParse( pubsub.callbackUrl ).pathname,
+    handle: function(req, res, next) {
+      if(req.method === 'POST') {
+        return  pubsub._onPostRequest( req, res);
+      } else if(req.method === 'GET') {
+        return pubsub._onGetRequest( req, res);
+      } else {
+        return pubsub._sendError(req, res, 405, "Method Not Allowed");
+      }
     }
-   });
-   pubsub.on( 'feed', data =>{
-     var stream = resumer().queue( data.feed ).end();
-     pubsub.emit( 'feedStream', stream, data.topic );
-   });
+  });
+  pubsub.on( 'feed', data =>{
+    var stream = resumer().queue( data.feed ).end();
+    pubsub.emit( 'feedStream', stream, data.topic );
+  });
 
 
-   return pubsub;
+  return pubsub;
 };
