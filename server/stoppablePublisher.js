0b65c72eb4e3570b335a7330d08d5f94055b8df9
mikowals <mikowals@gmail.com>
2015-01-26 20:56:46 +1000

    add files

---
+++ server/stoppablePublisher.js
@@ -0,0 +1,62 @@
+stoppablePublisher = function( sub ) {
+  this.sub = sub;
+};
+
+stoppablePublisher.prototype.subHasId = function( id ){
+  var name = this.name;
+  var sub = this.sub;
+  return sub._documents && sub._documents[ name ] && sub._documents[ name ][ id ];
+};
+
+stoppablePublisher.prototype.ids = function() {
+  var sub = this.sub;
+  return _.keys( sub._documents && sub._documents[ this.name ] || {} );
+};
+
+
+stoppablePublisher.prototype.observeAndPublish = function( cursor ) {
+  var self = this;
+  var oldIds = self.ids(),
+    name = self.name,
+    sub = self.sub;
+
+  self.handle = cursor.observeChanges({
+    added: function( id, doc ){
+      if ( self.subHasId( id ) ){
+        oldIds.splice( oldIds.indexOf( id ), 1);
+      } else{
+        sub.added( name, id, doc );
+      }
+    },
+    removed: function ( id ){
+      sub.removed( name, id );
+    },
+    changed: function ( id, doc ){
+      sub.changed( name, id, doc );
+    }
+  });
+
+  if ( sub._documents && oldIds.length){
+    oldIds.forEach( function( id ) { sub.removed ( name, id)} );
+  }
+};
+
+stoppablePublisher.prototype.start = function( cursor ){
+  var self = this;
+  var handle = self.handle;
+  var name = self.name;
+  if ( handle ) handle.stop();
+  if ( cursor._cursorDescription.collectionName !== name ){
+    if ( ! name )
+      this.name = cursor._cursorDescription.collectionName;
+    else
+      throw new Error( 'stoppablePublisher can not handle cursors from different collections. ',
+        name, ' to ', cursor._cursorDescription.collectionName);
+  }
+
+  self.observeAndPublish( cursor );
+};
+
+stoppablePublisher.prototype.stop = function() {
+  this.handle && this.handle.stop();
+};
