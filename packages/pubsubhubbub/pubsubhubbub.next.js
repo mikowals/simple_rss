42c5e5090a06d398253d604b0ec4091a81adb3e1
mikowals <mikowals@gmail.com>
2015-01-30 19:49:43 +1000

    refactor pubsub package

--- packages/pubsubhubbub/pubsubhubbub.next.js
+++ packages/pubsubhubbub/pubsubhubbub.next.js
@@ -1,80 +1,112 @@
 //based on and inspired by https://github.com/andris9/pubsubhubbub
 // customised to work with Meteor and within Simple_RSS app
 
-var PubSubHubbub = Npm.require("pubsubhubbub");
-var resumer = Npm.require("resumer");
-var urlParse = Npm.require( 'url' ).parse;
-var Future = Npm.require( 'fibers/future');
+var Stream = Npm.require( 'stream'),
+  inherits = Npm.require('util').inherits,
+  PubSubHubbub = Npm.require("pubsubhubbub"),
+  resumer = Npm.require("resumer"),
+  urlParse = Npm.require( 'url' ).parse,
+  Future = Npm.require( 'fibers/future');
 
-FeedSubscriber = (options) => {
-  var self = PubSubHubbub.createServer( options );
-  console.log( self );
+// a PuSH subcription manager that wraps
+FeedSubscriber = function ( options ) {
+  Stream.call( this );
+
+  var self = this;
+  self.server = PubSubHubbub.createServer( options );
+  var server = self.server;
+
+  if ( ! (self instanceof FeedSubscriber) )
+    throw new Error('use "new" to construct a FeedSubscriber');
 
   self.subscriptions = {};
 
   WebApp.connectHandlers.stack.splice(0,0,{
-    route: urlParse( self.callbackUrl ).pathname,
+    route: urlParse( server.callbackUrl ).pathname,
     handle: function(req, res, next) {
       if(req.method === 'POST') {
-        return  self._onPostRequest( req, res);
+        return  server._onPostRequest( req, res);
       } else if(req.method === 'GET') {
-        return self._onGetRequest( req, res);
+        return server._onGetRequest( req, res);
       } else {
-        return self._sendError(req, res, 405, "Method Not Allowed");
+        return server._sendError(req, res, 405, "Method Not Allowed");
       }
     }
   });
 
-  self.on( 'feed', data =>{
+  self.startEventListeners();
+};
+
+inherits( FeedSubscriber, Stream );
+
+// turn on and handle 'feed', 'subscribe', and 'unsubscribe' events
+FeedSubscriber.prototype.startEventListeners = function () {
+  var self = this;
+  var server = self.server;
+
+  server.on( 'feed', data =>{
     var stream = resumer().queue( data.feed ).end();
     self.emit( 'feedStream', stream, data.topic );
   });
 
-  self.stopAllSubscriptions = () => {
-    var waitingOn = _.map( self.subscriptions, sub => {
-      sub.unsub =  new Future();
-      self.unsubscribe( sub.url, sub.hub );
-      return sub.unsub;
-    });
-    return Future.wait( waitingOn );
-  };
-
-  self.on ( 'subscribe' , function ( data ) {
+  // check that subscribe events match our requests and manage subscriptions object
+  // create resubscription event 6 hours before expected expiry
+  server.on ( 'subscribe' , function ( data ) {
     var interval,
-        fn,
-        sub = self.subscriptions[ data.topic ];
+    fn,
+    sub = self.subscriptions[ data.topic ];
 
     if ( sub ){
-      //sub.expiry = new Date().getTime() + data.lease * 1000;
-      //sub.resub && clearTimeout( sub.resub );
-      console.log ( "subscribed to : " + data.hub + " : " + data.topic );
-      //interval = Math.max( ( data.lease - 60 * 60 ) * 1000, 9 * 24 * 60 * 60 * 1000 );
-      //fn = function(){
-      //  sub.unsub = new Future();
-      //  self.unsubscribe( data.topic, data.hub );
-      //  self.subscribe( data.topic, data.hub );
-      //};
-      //sub.resub = setTimeout( fn, interval );
+      sub.expiry = new Date( data.lease * 1000 );
+      sub.resub && clearTimeout( sub.resub );
+      console.log (["subscribed to : ", data.hub, " : ", data.topic, " : ", sub.expiry].join());
+      //  interval in ms with plan to resubscribe 6 hours before expiry
+      interval =  sub.expiry - new Date.getTime() - 6 * 60 * 60 * 1000;
+      sub.resub = setTimeout( function(){
+        sub.unsub = new Future();
+        server.unsubscribe( data.topic, data.hub );
+        server.subscribe( data.topic, data.hub );
+      }, interval );
     } else {
-      console.error( "unmatched subscription: " + data.topic);
+      console.error("unmatched subscription: " + data.topic);
     }
   });
 
+  // check that unsubscribe events match our requests and manage subscriptions object
   self.on ( 'unsubscribe' , function ( data ) {
+    var self = this;
     var sub = self.subscriptions[ data.topic ];
     if ( ! sub ){
       console.error( "unmatched unsubscribe: " + data.topic );
     }
     else if ( sub.unsub ){
-      if ( sub.unsub instanceof Future )
+      if (sub.unsub instanceof Future)
         sub.unsub.return( data );
-      console.log ( " unsubscribed from : " + data.topic);
+      console.log(" unsubscribed from : " + data.topic);
       delete self.subscriptions[ data.topic ];
     } else {
-      console.error( "resubscribing to: " + data.topic );
-      self.subscribe( data.topic, data.hub );
+      console.error("resubscribing to: " + data.topic);
+      server.subscribe( data.topic, data.hub );
     }
-  });
 
-  return self;
+  });
+};
+
+// allow user to stop all subscriptions with one synchronous call
+FeedSubscriber.prototype.stopAllSubscriptions = function () {
+  var self = this;
+  var waitingOn = _.map( self.subscriptions, sub => {
+    sub.unsub =  new Future();
+    self.server.unsubscribe( sub.url, sub.hub );
+    return sub.unsub;
+  });
+  return Future.wait( waitingOn );
+};
+
+FeedSubscriber.prototype.subscribe = function ( topic, hub ) {
+  this.server.subscribe( topic, hub );
+};
+
+FeedSubscriber.prototype.unsubscribe = function ( url, hub ) {
+  this.server.unsubscribe( url, hub );
 };
